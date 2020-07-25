f64aac0fdbd0e0e316a60c155093f399f4a86780
Richard Kellner <richard.kellner@gmail.com>
2020-07-25 22:44:13 +0200

    Releasing version 0.5.3

--- raspi_sensor/__init__.py
+++ raspi_sensor/__init__.py
@@ -1,10 +1,10 @@
 """
 RPi.Sensor
-Version: 0.5.2
+Version: 0.5.3
 Homepage: https://github.com/ricco386/RPi.Sensor
 
 This software is licensed as described in the README.rst and LICENSE files,
 which you should have received as part of this distribution.
 """
 
-__version__ = '0.5.2'
+__version__ = '0.5.3'
